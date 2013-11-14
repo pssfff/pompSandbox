7a101b01d9275d0c4f098a288a72a21178962cbe
Nicholas G Reich <nick@schoolph.umass.edu>
2013-11-14 14:44:07 -0500

    working example. R function exports when there is at least one valid C== exported function in the cpp file.

--- cppExample/ou2Example.cpp
+++ cppExample/ou2Example.cpp
@@ -9,6 +9,16 @@
 #include <Rcpp.h> // From Rcpp template
 using namespace Rcpp;
 
+// [[Rcpp::export]]
+NumericVector convolveCpp(NumericVector a, NumericVector b) {
+    int na = a.size(), nb = b.size();
+    int nab = na + nb - 1;
+    NumericVector xab(nab);
+for (int i = 0; i < na; i++)
+for (int j = 0; j < nb; j++)
+            xab[i + j] += a[i] * b[j];
+return xab; }
+
 /*** R
 
 procSim_R <- discrete.time.sim(
