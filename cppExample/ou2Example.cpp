1926e63a77e7546fde0ae8ab89a1d5caf37b93d6
Nicholas G Reich <nick@schoolph.umass.edu>
2013-11-14 14:19:08 -0500

    changing filename, adding R code.

--- cppExample/ou2Example.cpp
+++ cppExample/ou2Example.cpp
@@ -293,3 +293,21 @@ static double dens_ou2 (double *x1, double *x2,
   val = dnorm(eps[0],0.0,1.0,1)+dnorm(eps[1],0.0,1.0,1)-log(sigma1)-log(sigma3);
   return ((give_log) ? val : exp(val));
 }
+
+
+/*** R
+
+procSim_R <- discrete.time.sim(
+        step.fun=function (x, t, params, ...) {
+          eps <- rnorm(n=2,mean=0,sd=1) # noise terms
+          xnew <- c(
+                    x1=params["alpha.1"]*x["x1"]+params["alpha.3"]*x["x2"]+
+                        params["sigma.1"]*eps[1],
+                    x2=params["alpha.2"]*x["x1"]+params["alpha.4"]*x["x2"]+
+                        params["sigma.2"]*eps[1]+params["sigma.3"]*eps[2]
+                    )
+          names(xnew) <- c("x1","x2")
+xnew }
+        )
+        
+*/
