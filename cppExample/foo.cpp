0ff9750ca0da03b107bd7ad21e3bee0b72fdd8cc
Nicholas G Reich <nick@schoolph.umass.edu>
2013-11-18 15:04:14 -0500

    final(?) working foo example.

--- cppExample/foo.cpp
+++ cppExample/foo.cpp
@@ -1,13 +1,9 @@
 #include <Rcpp.h>
+
 using namespace Rcpp;
 
-// Below is a simple example of exporting a C++ function to R. You can
-// source this function into an R session using the Rcpp::sourceCpp 
-// function (or via the Source button on the editor toolbar)
-
-// For more on using Rcpp click the Help button on the editor toolbar
-
-//#define p (__k)
+// testing the define syntax at the top to abstract a variable call
+#define p (k)
 
 
 // [[Rcpp::export]]
@@ -17,5 +13,5 @@ int timesTwo(int x) {
 
 // [[Rcpp::export]]
 int timesK(int x, int k) {
-   return x * k;
+   return x * p;
 }
